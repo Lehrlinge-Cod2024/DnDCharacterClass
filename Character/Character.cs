ff11f58c5462b166885b8bb0a89ef75f8f37433d
Benn Mueller <f.benn.mueller@gmail.com>
2025-01-13 11:18:39 +0100

    implimentaion of most Variables for the Class

---
+++ Character/Character.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character
+{
+    internal class Character
+    {
+        public enum Class
+        {
+            Barbarian = 1,
+            Bard = 2,
+            Cleric = 3,
+            Druid = 4,
+            Fighter = 5,
+            Monk = 6,
+            Paladin = 7,
+            Ranger = 8,
+            Rough = 9,
+            Sorcerer = 10,
+            Warlock = 11,
+            Wizard = 12
+        }
+        [Flags]public enum Savings : uint // Saving Throws
+        {
+            Str = 1,
+            Dex = 2,
+            Con = 4,
+            Int = 8,
+            Wis = 16,
+            Cha = 32
+        }
+        [Flags]public enum SkillProfs: uint
+        {
+            Acrobatics = 1, // DEX
+            AnimalHandling = 2, // WIS
+            Arcana = 4, // INT
+            Athletics = 8, // STR
+            Deception = 16, // CHA
+            History = 32, // INT
+            Insight = 64, // WIS
+            Intimidation = 128, // CHA
+            Investigation = 256, // INT
+            Medicine = 512, // WIS
+            Nature = 1024, // CHA
+            Perception = 2048, // INT 
+            Performance = 4096, // WIS
+            Persuation = 8192, // CHA
+            Religion = 16384, // INT
+            SleightOfHand = 32768, // DEX
+            Stealth = 65536, // DEX
+            Survival = 131072 // WIS
+        }
+        public int StatStr { get; set; }
+        public int StatDex { get; set; }
+        public int StatCon { get; set; }
+        public int StatInt { get; set; }
+        public int StatWis { get; set; }
+        public int StatCha { get; set; }
+        public int ProfBonus { get; set; }
+        public int Level { get; set; }
+        public int Xp { get; set; }
+        public int PassivWis { get; set; }
+        public int AC { get; set; }
+        public int Init { get; set; }
+        public int Speed { get; set; }
+        public int HpMax { get; set; }
+        public int HpCurrent { get; set; }
+        public int HpTemp { get; set; }
+        public string HitDice { get; set; }
+        public string Race { get; set; }
+        public string CharBackground { get; set; }
+        public string PlayerName { get; set; }
+        public string CharName { get; set; }
+        public string Alignment { get; set; }
+        public int Inspiration { get; set; }
+        public SkillProfs Skills { get; set; }
+        public Savings SavingThrows { get; set; }
+
+
+    }
+
+}
